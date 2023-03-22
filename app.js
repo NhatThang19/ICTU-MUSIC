bd5b76f1f5f84baf491346ff81e700d1b7960ddb
La Nhat Thang <lanhatthang1910@gmail.com>
2023-03-22 16:48:46 +0700

    Up load code

--- app.js
+++ app.js
@@ -21,3 +21,42 @@ document.addEventListener('DOMContentLoaded', function () {
         autoplay: true,
       }).mount();
   });
+
+  const notifications = document.querySelector('.notifications')
+  const modal = document.querySelector('.modal')
+  const modalContainer = document.querySelector('.modal_container')
+
+ 
+  function showNotifications() {
+    modal.classList.add('open')
+  }
+
+  function hideNotifications() {
+    modal.classList.remove('open')
+  }
+
+  notifications.addEventListener('click', showNotifications)
+  modal.addEventListener('click', hideNotifications)
+
+  modalContainer.addEventListener('click', function (event){
+    event.stopPropagation()
+  })
+
+  const profile = document.querySelector('.profile')
+  const modal01 = document.querySelector('.modal_01')
+  const modalprofile = document.querySelector('.modal-profile')
+
+  function showProfile() {
+    modal01.classList.add('open')
+  }
+
+  function hideProfile() {
+    modal01.classList.remove('open')
+  }
+
+  profile.addEventListener('click', showProfile)
+  modal01.addEventListener('click', hideProfile)
+
+  modalprofile.addEventListener('click', function (event){
+    event.stopPropagation()
+  })
